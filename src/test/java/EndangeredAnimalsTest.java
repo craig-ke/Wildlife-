d73eb301afe86754db12c5edc2335689826c6463
craig-ke <lawremce.olonyi.67@gmail.com>
2020-12-09 01:39:55 +0300

    all instances are saves passes

--- src/test/java/EndangeredAnimalsTest.java
+++ src/test/java/EndangeredAnimalsTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class EndangeredAnimalsTest {
     @Test
@@ -8,6 +9,23 @@ public class EndangeredAnimalsTest {
         EndangeredAnimals testAnimal= setUpNewAnimal();
         assertEquals(true,testAnimal instanceof EndangeredAnimals);
     }
+
+    @Test
+    public void allInstancesAreSaved(){
+        EndangeredAnimals testAnimal=setUpNewAnimal();
+        testAnimal.save();
+        assertEquals(EndangeredAnimals.all().get(0).getHealth(), testAnimal.getHealth());
+    }
+//
+//    @Test
+//    public void findByIdReturnsCorrectInfo(){
+//        EndangeredAnimals testAnimal=setUpNewAnimal();
+//        testAnimal.save();
+//        Animals foundAnimal= Animals.find(testAnimal.getId());
+//        assertTrue(foundAnimal.getHealth().equals(testAnimal.getHealth()));
+//
+//    }
+
     private EndangeredAnimals setUpNewAnimal() {
         return new EndangeredAnimals("Albino Giraffe","endangered","healthy","young");
     }
