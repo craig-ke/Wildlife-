0f3e0798c094a3684e8376ef9fd80599793b09c0
craig-ke <lawremce.olonyi.67@gmail.com>
2020-12-08 21:07:12 +0300

    entry update ensured passes

--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -15,7 +15,20 @@ public class AnimalsTest {
         testAnimal.save();
         assertTrue(Animals.all().get(0).equals(testAnimal));
     }
+    @Test
+    public void ensureEntryIsUpdatedCorrectly() {
+        Animals testAnimal=setUpNewAnimal();
+        Animals otherAnimal=testAnimal;
+        testAnimal.save();
+        try {
+            testAnimal.update(testAnimal.getId(),"endangered","ill","newborn");
+            Animals updatedAnimal=  Animals.find(testAnimal.getId());
+            assertEquals(updatedAnimal.getId(),otherAnimal.getId());
+            assertNotEquals(updatedAnimal.getHealth(),otherAnimal.getHealth());
+        }catch (IllegalArgumentException e){
 
+        }
+    }
     private Animals setUpNewAnimal() {
         return new Animals ( "Antelope","normal");
     }
